0dc2fe9463d1f91e42e5ea35265abce65ab9733f
William Gomes <williamlimagomes62@gmail.com>
2022-12-20 15:18:59 -0300

    chore: order header

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,13 +1,39 @@
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
-import { Container } from './styles';
-export function Header() {
+import { Container, Content, OrderHeader, Table } from './styles';
+
+interface HeaderProps {
+  selectedTable: string;
+  onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>Bem vindo(a) ao</Text>
-      <Text size={24} weight='600'>
-        WAITER
-        <Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>Bem vindo(a) ao</Text>
+          <Text size={24} weight='600'>
+            WAITER
+            <Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+      {selectedTable && (
+        <Content>
+          <OrderHeader>
+            <Text size={24} weight='600'>Pedido</Text>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <Text color='#D73035' weight='600' size={14}>
+                Cancelar Pedido
+              </Text>
+            </TouchableOpacity>
+          </OrderHeader>
+          <Table>
+            <Text color='#666'>Mesa {selectedTable}</Text>
+          </Table>
+        </Content>
+      )}
     </Container>
   );
 }
