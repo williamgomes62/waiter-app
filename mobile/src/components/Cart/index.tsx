1ffaca0c22c5ad74143606a5862b9478be6115d7
William Gomes <williamlimagomes62@gmail.com>
2022-12-21 14:28:07 -0300

    chore: cart summary

--- mobile/src/components/Cart/index.tsx
+++ mobile/src/components/Cart/index.tsx
@@ -1,10 +1,11 @@
 import { FlatList, TouchableOpacity } from 'react-native';
 import { CartItem } from '../../types/CartItem';
 import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Buttons';
 import { MinusCircle } from '../Icons/MinusCircle';
 import { PlusCircle } from '../Icons/PlusCircle';
 import { Text } from '../Text';
-import { Actions, Image, Item, ProductContainer, ProductDetails, QuantityContainer } from './styles';
+import { Actions, Image, Item, ProductContainer, ProductDetails, QuantityContainer, Summary, TotalContainer } from './styles';
 
 interface CartProps {
   cartItems: CartItem[];
@@ -13,36 +14,59 @@ interface CartProps {
 export function Cart({ cartItems }: CartProps) {
 
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={cartItem => cartItem.product._id}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item: cartItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image
-              source={require('../../assets/1668645450167-quatro-queijos.png')}
-            />
-            <QuantityContainer>
-              <Text size={14} color='#666'>{cartItem.quantity}x</Text>
-            </QuantityContainer>
-            <ProductDetails>
-              <Text size={14} weight='600'>{cartItem.product.name}</Text>
-              <Text size={14} color='#666' style={{ marginTop: 4 }}>
-                {formatCurrency(cartItem.product.price)}
-              </Text>
-            </ProductDetails>
-          </ProductContainer>
-          <Actions>
-            <TouchableOpacity style={{ marginRight: 20 }}>
-              <PlusCircle />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <MinusCircle />
-            </TouchableOpacity>
-          </Actions>
-        </Item>
+    <>
+      {cartItems.length > 0 && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={cartItem => cartItem.product._id}
+          showsVerticalScrollIndicator={false}
+          style={{ marginBottom: 20, maxHeight: 150 }}
+          renderItem={({ item: cartItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={require('../../assets/1668645450167-quatro-queijos.png')}
+                />
+                <QuantityContainer>
+                  <Text size={14} color='#666'>{cartItem.quantity}x</Text>
+                </QuantityContainer>
+                <ProductDetails>
+                  <Text size={14} weight='600'>{cartItem.product.name}</Text>
+                  <Text size={14} color='#666' style={{ marginTop: 4 }}>
+                    {formatCurrency(cartItem.product.price)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
+              <Actions>
+                <TouchableOpacity style={{ marginRight: 20 }}>
+                  <PlusCircle />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <MinusCircle />
+                </TouchableOpacity>
+              </Actions>
+            </Item>
+          )}
+        />
       )}
-    />
+      <Summary>
+        <TotalContainer>
+          {cartItems.length > 0 ? (
+            <>
+              <Text color='#666'>Total</Text>
+              <Text size={20} weight='600'>{formatCurrency(54)}</Text>
+            </>
+          ) : (
+            <Text color='#999'>Seu carrinho está vazio</Text>
+          )}
+        </TotalContainer>
+        <Button
+          onPress={() => alert('confirmar pedido')}
+          disabled={cartItems.length === 0}
+        >
+          Confirmar Pedido
+        </Button>
+      </Summary>
+    </>
   );
 }
