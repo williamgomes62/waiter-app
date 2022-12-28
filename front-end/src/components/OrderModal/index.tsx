f25ea38b2fa3e2d18866fb2804f3c2c0c664ec6d
William Gomes <williamlimagomes62@gmail.com>
2022-12-28 13:28:01 -0300

    feat: change order status

--- front-end/src/components/OrderModal/index.tsx
+++ front-end/src/components/OrderModal/index.tsx
@@ -10,6 +10,7 @@ interface OrderModalProps {
   onClose: () => void;
   onCancelOrder: () => Promise<void>;
   isLoading: boolean;
+  onChangeOrderStatus: () => void;
 }
 
 export function OrderModal({
@@ -17,7 +18,8 @@ export function OrderModal({
   order,
   onClose,
   onCancelOrder,
-  isLoading
+  isLoading,
+  onChangeOrderStatus
 }: OrderModalProps) {
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
@@ -90,14 +92,23 @@ export function OrderModal({
           </div>
         </OrderDatails>
         <Actions>
-          <button
-            type='button'
-            className='primary'
-            disabled={isLoading}
-          >
-            <span>⌛</span>
-            <strong>Iniciar Produção</strong>
-          </button>
+          {order.status !== 'DONE' && (
+            <button
+              type='button'
+              className='primary'
+              disabled={isLoading}
+              onClick={onChangeOrderStatus}
+            >
+              <span>
+                {order.status === 'WAITING' && '👨‍🍳'}
+                {order.status === 'IN_PRODUCTION' && '✅'}
+              </span>
+              <strong>
+                {order.status === 'WAITING' && 'Iniciar Produção'}
+                {order.status === 'IN_PRODUCTION' && 'Concluir Pedido'}
+              </strong>
+            </button>
+          )}
           <button
             type='button'
             className='secundary'
--- front-end/src/components/Orders/index.tsx
+++ front-end/src/components/Orders/index.tsx
@@ -22,6 +22,14 @@ export function Orders() {
     setOrders((prevState) => prevState.filter(order => order._id !== orderId));
   }
 
+  function handleOrderStatusChange(orderId: string, status: Order['status']) {
+    setOrders((prevState) => prevState.map((order) => (
+      order._id === orderId
+        ? { ...order, status }
+        : order
+    )));
+  }
+
   return (
     <Container>
       <OrdersBoard
@@ -29,18 +37,21 @@ export function Orders() {
         title='Fila de espera'
         orders={waiting}
         onCancelOrder={handleCancelOrder}
+        onChangeOrderStatus={handleOrderStatusChange}
       />
       <OrdersBoard
         icon='⌛'
         title='Em preparação'
         orders={inProduction}
         onCancelOrder={handleCancelOrder}
+        onChangeOrderStatus={handleOrderStatusChange}
       />
       <OrdersBoard
         icon='✅'
         title='Pronto'
         orders={done}
         onCancelOrder={handleCancelOrder}
+        onChangeOrderStatus={handleOrderStatusChange}
       />
     </Container>
   );
