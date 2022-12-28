598df81523e7a6f338776aaa1734afb78f8dc773
William Gomes <williamlimagomes62@gmail.com>
2022-12-28 11:15:06 -0300

    chore: add toast message on cancel order

--- front-end/src/components/OrdersBoard/index.tsx
+++ front-end/src/components/OrdersBoard/index.tsx
@@ -3,6 +3,7 @@ import { Order } from '../../types/Order';
 import { api } from '../../utils/api';
 import { OrderModal } from '../OrderModal';
 import { Board, OrderContainer } from './styles';
+import { toast } from 'react-toastify';
 
 interface OrdersBoardProps {
   icon: string;
@@ -38,6 +39,7 @@ export function OrdersBoard({ icon, title, orders, onCancelOrder }: OrdersBoardP
     await new Promise(resolve => setTimeout(resolve, 3000));
     await api.delete(`/orders/${selectedOrder?._id}`);
 
+    toast.success(`O pedido da mesa ${selectedOrder!.table} foi cancelado!`);
     onCancelOrder(selectedOrder!._id);
     setIsLoading(false);
     setIsModalVisible(false);
